bb32cfac37664a398d45605ca56d72a7068a4b70
Efe Ağlamazlar <aglamazlarefe@gmail.com>
2026-08-13 01:33:26 +0300

    feat: add Firebase client, Serwist PWA support, and update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import withSerwist from '@serwist/next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+const config =
+  process.env.NODE_ENV === 'production'
+    ? withSerwist({
+        swSrc: 'src/sw.ts',
+        swDest: 'public/sw.js',
+        cacheOnNavigation: true,
+      })(nextConfig)
+    : nextConfig;
+
+export default config;
